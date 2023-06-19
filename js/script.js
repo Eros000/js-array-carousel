cfd6379d95d5e8f4c155eef086d5d65d69d711f6
Eros000 <e.ghilardi@hotmail.it>
2023-06-19 11:14:44 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-const img1 = document.createElement('div');
 
-img1.innerHTML = img1 + `<img src="$(../img/01.webp)"`;
+const images = [
+    './img/01.webp',
+    './img/02.webp',
+    './img/03.webp',
+    './img/04.webp',
+    './img/05.webp',
+];
 
-console.log('img1', img1, typeof img1);
+const itemsContainer = document.querySelector('.container');
 
+for (let i = 0; i < images.length; i++) {
+    console.log(i, images [i]);
+
+    if(i == 0){
+        itemsContainer.innerHTML += `
+            <div class="img-container my-img active">
+                <img src="${images[i]}" alt="">
+            </div>
+        `
+    }
+    else{
+        itemsContainer.innerHTML += `
+            <div class="img-container my-img">
+                <img src="${images[i]}" alt="">
+            </div>
+        `
+    }
+
+}
+        
