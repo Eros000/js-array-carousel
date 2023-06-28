f07631ef187bd1e952aaf7d6d6b09f6da7052df4
Eros000 <e.ghilardi@hotmail.it>
2023-06-28 16:30:39 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -7,25 +7,65 @@ const images = [
     './img/05.webp',
 ];
 
+let activeId = 0;
+
 const itemsContainer = document.querySelector('.container');
 
+//CREO LE IMMAGINI IN PAGINA
 for (let i = 0; i < images.length; i++) {
     console.log(i, images [i]);
 
     if(i == 0){
         itemsContainer.innerHTML += `
-            <div class="img-container my-img active">
+            <div class="img-container my-img active" id="img_${i}">
                 <img src="${images[i]}" alt="">
             </div>
         `
     }
     else{
         itemsContainer.innerHTML += `
-            <div class="img-container my-img">
+            <div class="img-container my-img" id="img_${i}">
                 <img src="${images[i]}" alt="">
             </div>
         `
     }
-
 }
-        
+
+const myButtonNext = document.querySelector('.next')
+
+myButtonNext.addEventListener( 'click',function(){
+
+    const activeImage = document.getElementById('img_'+activeId);
+    
+    if(activeId < images.length-1){    
+        activeId++;
+    }else{
+        activeId = 0;
+    }
+
+    const nextImage = document.getElementById('img_'+activeId);
+
+    activeImage.classList.remove('active');
+    nextImage.classList.add('active');
+    
+})
+
+const myButtonPrev = document.querySelector('.prev')
+
+myButtonPrev.addEventListener( 'click',function(){
+
+    const activeImage = document.getElementById('img_'+activeId);
+    
+    if(activeId > 0){    
+        activeId--;
+    }else{
+        activeId = images.length-1;
+    }
+
+    const nextImage = document.getElementById('img_'+activeId);
+
+    activeImage.classList.remove('active');
+    nextImage.classList.add('active');
+    
+})
+
